044f9ecfedf245735bf3be0d931898577c194116
jiangminxi <jiangminxi@yy.com>
2023-08-17 17:07:09 +0800

    feat: add ant-design

--- src/script/popup.js
+++ src/script/popup.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Popup from './views/Popup'
+import { Slider, Switch } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Slider)
+Vue.use(Switch)
+
 new Vue({
   render: h => h(Popup),
   mounted () {
--- src/script/views/Popup/index.vue
+++ src/script/views/Popup/index.vue
@@ -3,10 +3,31 @@
     <div>选择图片</div>
     <input type="file" id="imgUpload" accept="image/*" />
     <button class="btn1">测试</button>
+    <a-slider v-model="opacity" :min="0.01" :max="1" :default-value="30" />
+    <a-switch v-model="enable" />
   </div>
 </template>
 <script>
+import debounce from 'lodash/debounce'
+const updateSlider = debounce((callback) => {
+  callback()
+}, 300)
 export default {
+  data () {
+    return {
+      opacity: 0.8,
+      enable: false
+    }
+  },
+  watch: {
+    opacity (val) {
+      const that = this
+      updateSlider(async () => {
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.tabs.sendMessage(tab.id, {action:'opacity' , payload: that.opacity})
+      })
+    }
+  },
   mounted () {
     document.getElementById('imgUpload').addEventListener('change', (event) => {
       const file = event.target.files[0]
